37c9525686c3961e7c5fa544af11b787f57123a7
asrikanth29 <69597613+asrikanth29@users.noreply.github.com>
2021-12-02 17:21:15 +0530

    Update MicroK8s-worker-setup.sh

--- MicroK8s-worker-setup.sh
+++ MicroK8s-worker-setup.sh
@@ -5,4 +5,4 @@ echo "Install MicroK8s with kubernetes version 1.20"
 sudo snap install microk8s --classic --channel=1.20/stable
 echo "waiting to confirm installation"
 sudo microk8s status --wait-ready
-echo "now run join link which is generated on master node, if not yet generated run command: sudo microk8s add-node "
+echo "now run join link which is generated on master node, if not yet generated run command: sudo microk8s add-node    on master node "
