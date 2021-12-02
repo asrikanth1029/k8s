fb3131de7eba9e4c74e66c3835a59c07b1d39292
asrikanth29 <69597613+asrikanth29@users.noreply.github.com>
2021-12-02 16:31:15 +0530

    Create MicroK8s-worker-setup.sh

---
+++ MicroK8s-worker-setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#this script is idempotent. Its ok to run it multiple times while troubleshooting any errors
+echo "watch the screen carefully for any errors"
+echo "Install MicroK8s with kubernetes version 1.20"
+sudo snap install microk8s --classic --channel=1.20/stable
+echo "waiting to confirm installation"
+sudo microk8s status --wait-ready
+echo "now run join link which is generated on master node, if not yet generated run command: sudo microk8s add-node "
