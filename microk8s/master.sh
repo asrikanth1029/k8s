b55501e30460a348780d387e17cced58199c717d
asrikanth29 <69597613+asrikanth29@users.noreply.github.com>
2021-12-03 16:59:34 +0530

    Update master.sh

--- microk8s/master.sh
+++ microk8s/master.sh
@@ -7,7 +7,7 @@ echo "Enabling admin privileges to microk8s"
 sudo usermod -a -G microk8s $USER
 sudo chown -f -R $USER ~/.kube
 echo "Enabling enable dashboard, dns, registry, services"
-sudo microk8s enable dashboard dns registry
+sudo microk8s enable dashboard dns registry istio
 echo "let us add nodes to this cluster"
 sudo microk8s add-node
 echo "to add 2 nodes to this cluster ,two joining links are created above note down the above join links like microk8s join ip:port/xxxxxx you neet is in later step "
