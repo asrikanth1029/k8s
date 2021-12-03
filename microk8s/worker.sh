882abfa13c3840a71e3a5440ef7bacd1272e6268
asrikanth29 <69597613+asrikanth29@users.noreply.github.com>
2021-12-03 16:44:26 +0530

    Update worker.sh

--- microk8s/worker.sh
+++ microk8s/worker.sh
@@ -1,6 +1,4 @@
 cd ./k8s
-sudo chmod 755 MicroK8s-worker-setup.sh
-echo "watch the screen carefully for any errors"
 echo "Installing MicroK8s with kubernetes version 1.20"
 sudo snap install microk8s --classic --channel=1.20/stable
 echo "waiting to confirm installation"
